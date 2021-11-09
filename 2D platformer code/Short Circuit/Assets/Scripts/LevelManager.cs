f40ced6a87a418f9bf6691fd63ea60b1c0b6f579
mominicDyerchin <myerchindominic@gmail.com>
2021-11-08 17:04:33 -0800

    fixing falling off screen

--- 2D platformer code/Short Circuit/Assets/Scripts/LevelManager.cs
+++ 2D platformer code/Short Circuit/Assets/Scripts/LevelManager.cs
@@ -14,6 +14,9 @@ public class LevelManager : MonoBehaviour
     public int pointsPenaltyOnDeath;
 
     public float respawnTime;
+
+    private float gravityStore;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,10 +40,13 @@ public class LevelManager : MonoBehaviour
         player.enabled = false;
         player.GetComponent<Renderer>().enabled = false;
         player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        gravityStore = player.GetComponent<Rigidbody2D>().gravityScale;
+        player.GetComponent<Rigidbody2D>().gravityScale = 0f;
         ScoreManager.AddPoints(-pointsPenaltyOnDeath);
         Debug.Log("Player Respawn");
         yield return new WaitForSeconds(respawnTime);
         player.transform.position = currentCheckpoint.transform.position;
+        player.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
         player.enabled = true;
         player.GetComponent<Renderer>().enabled = true;
         Instantiate(respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
